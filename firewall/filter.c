f01858bd5e8c7ef71cca80ea5a4eab72c6561b4a
Chathan Driehuys <chathan@driehuys.com>
2019-04-12 18:36:31 -0400

    Finish implementing five firewall rules.

--- firewall/filter.c
+++ firewall/filter.c
@@ -7,8 +7,11 @@
 #include <linux/udp.h>
 #include <linux/inet.h>
 
+#define EXAMPLE_COM_IP "93.184.216.34"
 #define MACHINE_B "10.0.2.6"
-#define TCP_PROTO 6 
+#define SSH_PORT 22
+#define SYR_IP "128.230.18.198"
+#define TCP_PROTO 6
 #define TELNET_PORT 23
 
 
@@ -24,18 +27,25 @@ unsigned int pre_routing_hook(void *priv, struct sk_buff *skb, const struct nf_h
 
     // If it's a TCP request:
     if (ip_header->protocol==TCP_PROTO) {
-	struct tcphdr *tcp_header = (struct tcphdr*)skb_transport_header(skb);
-	unsigned int src_port = (unsigned int)ntohs(tcp_header->source);
-	unsigned int dest_port = (unsigned int)ntohs(tcp_header->dest);
+    	struct tcphdr *tcp_header = (struct tcphdr*)skb_transport_header(skb);
+    	unsigned int src_port = (unsigned int)ntohs(tcp_header->source);
+    	unsigned int dest_port = (unsigned int)ntohs(tcp_header->dest);
 
         // Block incoming telnet from Machine B
-	if (src_ip == in_aton(MACHINE_B) && dest_port == TELNET_PORT) {
+    	if (src_ip == in_aton(MACHINE_B) && dest_port == TELNET_PORT) {
             printk(KERN_DEBUG "Dropping packet from %pI4:%d to %pI4:%d\n", &src_ip, src_port, &dest_ip, dest_port);
 
             return NF_DROP;
-	}
+    	}
+
+        // Block incoming SSH from Machine B
+        if (src_ip == in_aton(MACHINE_B) && dest_port == SSH_PORT) {
+            printk(KERN_DEBUG "Dropping SSH packet from %pI4:%d", &src_ip, src_port);
+
+            return NF_DROP;
+        }
     }
-    
+
     // Default accept
     return NF_ACCEPT;
 }
@@ -48,10 +58,24 @@ unsigned int post_routing_hook(void *priv, struct sk_buff *skb, const struct nf_
         struct tcphdr *tcp_header = (struct tcphdr*) skb_transport_header(skb);
         unsigned int dest_ip = (unsigned int) ip_header->daddr;
         unsigned int dest_port = (unsigned int) ntohs(tcp_header->dest);
-	
+
         // Block outgoing telnet packets to Machine B
         if (dest_ip == in_aton(MACHINE_B) && dest_port == TELNET_PORT) {
             printk(KERN_DEBUG "Dropping packet to %pI4:%d\n", &dest_ip, dest_port);
+
+            return NF_DROP;
+        }
+
+    	// Block outgoing packets to example.com
+    	if (dest_ip == in_aton(EXAMPLE_COM_IP)) {
+    	    printk(KERN_DEBUG "Dropping packet intended for 'example.com'\n");
+
+    	    return NF_DROP;
+    	}
+
+        // Block outgoing packets to syr.edu
+        if (dest_ip == in_aton(SYR_IP)) {
+            printk(KERN_DEBUG "Dropping packet intended for 'syr.edu'\n");
 
             return NF_DROP;
         }
@@ -64,7 +88,7 @@ unsigned int post_routing_hook(void *priv, struct sk_buff *skb, const struct nf_
 int init_module() {
     printk(KERN_INFO "Loading custom filter module...\n");
     unsigned int machine_b_ip = in_aton(MACHINE_B);
-    printk(KERN_DEBUG "Machine B IP: %pI4\n", &machine_b_ip); 
+    printk(KERN_DEBUG "Machine B IP: %pI4\n", &machine_b_ip);
 
     // Fill in incoming hook structure
     incoming_hook.hook = pre_routing_hook;
